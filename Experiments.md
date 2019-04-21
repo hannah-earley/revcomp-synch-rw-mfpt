ef4933e23dab078d4bb30ab54fd402331238814c
Hannah Earley <h@nnah.io>
2019-04-21 21:32:53 +0100

    experimental planning

---
+++ Experiments.md
@@ -0,0 +1,5 @@
+# Experiment Plan
+
+## Width=1 constant, bias x distance
+- bias: 0.1 0.01 0.001
+- dist: 1 3 10 32 100 316 1000 3162 10000 31623 100000
---
+++ job.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+bias=1
+dist=1
+widt=1
+n=1000
+m=1000
+s=1000
+
+while getopts "b:d:w:n:m:s:" o; do
+    case "${o}" in
+        b)
+            bias="${OPTARG}"
+            ;;
+        d)
+            dist="${OPTARG}"
+            ;;
+        w)
+            widt="${OPTARG}"
+            ;;
+        n)
+            n="${OPTARG}"
+            ;;
+        m)
+            m="${OPTARG}"
+            ;;
+        s)
+            s="${OPTARG}"
+            ;;
+    esac
+done
+
+dir="./jobs"
+file="2d-$bias-$widt-$dist"
+log="$dir/$file.log"
+dat="$dir/$file.dat"
+
+mkdir -p "$dir"
+echo "#" ./walk -2 -v -b "$bias" -d "$dist" -w "$widt" \
+     -n "$n" -m "$m" -s "$s" -p "$dat" >> "$log"
+./walk -2 -v -b "$bias" -d "$dist" -w "$widt" -n "$n" \
+       -m "$m" -s "$s" -p "$dat" 2>&1 | tee -a "$log"
