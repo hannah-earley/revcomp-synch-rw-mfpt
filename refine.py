16ce43f126f6fce2f2142fbfdc7f36141e7f8d6b
Hannah Earley <h@nnah.io>
2019-04-25 13:43:27 +0100

    refine.py: support for multiple files

--- refine.py
+++ refine.py
@@ -4,37 +4,27 @@ import math
 
 COMMENT_LINE='#'
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument('-s', '--skip', action='store', default=0, type=int,
-                        help='number of output datums to skip')
-    parser.add_argument('file', type=argparse.FileType('r'),
-                        help='(clean) output file')
-
-    args = parser.parse_args()
-    skip = args.skip
-
+def refine(file, skip=0):
     agg_mean = 0
     agg_err = 0
     agg_w = 0
 
-    with args.file as file:
-        for line in file:
-            line = line.strip()
-            if line.startswith(COMMENT_LINE):
-                continue
-            if skip > 0:
-                skip -= 1
-                continue
+    for line in file:
+        line = line.strip()
+        if line.startswith(COMMENT_LINE):
+            continue
+        if skip > 0:
+            skip -= 1
+            continue
 
-            mean, err, w = line.split(',')
-            mean = float(mean)
-            err = float(err)
-            w = int(w)
+        mean, err, w = line.split(',')
+        mean = float(mean)
+        err = float(err)
+        w = int(w)
 
-            agg_mean += w * mean
-            agg_err += w * w * err * err
-            agg_w += w
+        agg_mean += w * mean
+        agg_err += w * w * err * err
+        agg_w += w
 
     agg_mean /= agg_w
     agg_err = math.sqrt(agg_err)
@@ -43,5 +33,23 @@ if __name__ == '__main__':
     inv_mean = 1.0 / agg_mean
     inv_err = agg_err * inv_mean * inv_mean
 
-    # print(f"{agg_mean} (±{agg_err})")
-    print("mean: %r (±%r)" % (inv_mean, inv_err))
+    return inv_mean, inv_err
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('-s', '--skip', action='store', default=0, type=int,
+                        help='number of output datums to skip')
+    parser.add_argument('file', type=argparse.FileType('r'), nargs='*',
+                        help='(clean) output file')
+
+    args = parser.parse_args()
+    skip = args.skip
+
+    for fa in args.file:
+        with fa as file:
+            try:
+                inv_mean, inv_err = refine(file, skip)
+                print("%s mean: %r (±%r)" % (file.name, inv_mean, inv_err))
+            except:
+                print("%s error" % (file.name,))
