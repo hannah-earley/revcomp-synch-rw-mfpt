5d227787da3004f449e38b5321f9a09636e66cd7
Hannah Earley <h@nnah.io>
2019-04-29 14:10:35 +0100

    csv option for refine.py

--- refine.py
+++ refine.py
@@ -35,21 +35,31 @@ def refine(file, skip=0):
 
     return inv_mean, inv_err
 
+def csv_text(x):
+    allowed = set(list("abcdefghijklmnopqrstuvwxyz0123456789-_ "))
+    chars = set(list(x.lower()))
+    if chars.issubset(allowed):
+        return x
+    return '"%s"' % x.encode('unicode_escape').replace(b'"', b'\\"').decode('ascii')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('-s', '--skip', action='store', default=0, type=int,
                         help='number of output datums to skip')
+    parser.add_argument('-c', '--csv', action='store_true', default=False)
     parser.add_argument('file', type=argparse.FileType('r'), nargs='*',
                         help='(clean) output file')
 
     args = parser.parse_args()
     skip = args.skip
+    csv = args.csv
 
     for fa in args.file:
         with fa as file:
             try:
                 inv_mean, inv_err = refine(file, skip)
-                print("%s mean: %r (±%r)" % (file.name, inv_mean, inv_err))
-            except:
-                print("%s error" % (file.name,))
+                if csv: print("%s,ok,%r,%r"       % (csv_text(file.name), inv_mean, inv_err))
+                else:   print("%s mean: %r (±%r)" % (file.name, inv_mean, inv_err))
+            except Exception as e:
+                if csv: print("%s,error,%s," % (csv_text(file.name), csv_text(repr(e))))
+                else:   print("%s error: %r" % (file.name, e))
