5cebe01d3978d99cceec8391f240738a183530b7
Hannah Earley <h@nnah.io>
2019-05-03 17:49:27 +0100

    improve help, move some parameters into config.py

---
+++ config.template.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Email configuration for batch status updates
+class email:
+    recipient = 'Me <user@localhost>'
+    sender = 'RWX Batch <user@localhost>'
+    subject = 'Re: RWX Batch Update'
