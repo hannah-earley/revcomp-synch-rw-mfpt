ab2d5943c1920fec0b481fc14c8ddd12dfc04172
William Earley <william@earley.org.uk>
2019-05-21 12:55:41 +0100

    add diff capability to lcs, optimise in space

--- common.py
+++ common.py
@@ -218,57 +218,6 @@ class CommonParser(argparse.ArgumentParser):
         self.set_defaults(handler=handler)
         return handler
 
-"""
--- longest common subsequence algorithm
--- inefficient - improve by memoising lcs...
-
-lcs :: Ord a => [a] -> [a] -> [[a]]
-lcs (x:xs) (y:ys)
-  | x == y = map (x:) (lcs xs ys)
-  | x /= y = longest $ mergeSet (lcs (x:xs) ys) (lcs xs (y:ys))
-lcs _ _ = [[]]
-
-mergeSet :: Ord a => [a] -> [a] -> [a]
-mergeSet [] xs = xs
-mergeSet xs [] = xs
-mergeSet (x:xs) (y:ys) = case compare x y of
-    LT -> x : mergeSet xs (y:ys)
-    EQ -> x : mergeSet xs ys
-    GT -> y : mergeSet (x:xs) ys
-
-longest :: [[a]] -> [[a]]
-longest = go 0 []
-  where
-    go _ yss [] = reverse yss
-    go n yss (xs:xss) =
-      let l = length xs in
-      case compare l n of
-        LT -> go n yss xss
-        EQ -> go n (xs:yss) xss
-        GT -> go l [xs] xss
-"""
-
-def longest_subseq_str(xs, ys, cmp=lambda a,b:a==b):
-    lx = len(xs)
-    ly = len(ys)
-
-    memo = [[None for _ in range(ly+1)] for _ in range(lx+1)]
-    for i in range(lx+1):
-        memo[i][0] = {""}
-    for j in range(ly+1):
-        memo[0][j] = {""}
-
-    for i,x in enumerate(xs):
-        for j,y in enumerate(ys):
-            if cmp(x, y):
-                memo[i+1][j+1] = {zs+x for zs in memo[i][j]}
-            else:
-                zss = memo[i+1][j].union(memo[i][j+1])
-                lz = max(map(len, zss))
-                memo[i+1][j+1] = {zs for zs in zss if len(zs) == lz}
-
-    return memo[lx][ly]
-
 class peekable:
     def __init__(self, xi):
         self._iter = iter(xi)
@@ -307,50 +256,155 @@ class peekable:
         except StopIteration:
             return False, None
 
-def mergeSet(xs, ys):
-    xi = peekable(xs)
-    yi = peekable(ys)
+class LCS:
+    def __init__(self, xs, ys):
+        self.xs = xs
+        self.ys = ys
+        self.compute3()
 
-    while True:
-        xp, x = xi.safe_peek()
-        yp, y = yi.safe_peek()
+    @staticmethod
+    def mergeSet(xs, ys):
+        xi = peekable(xs)
+        yi = peekable(ys)
 
-        if xp:
-            if yp:
-                if x < y:
-                    yield next(xi)
-                elif x == y:
-                    yield next(xi)
-                    next(yi)
+        while True:
+            xp, x = xi.safe_peek()
+            yp, y = yi.safe_peek()
+
+            if xp:
+                if yp:
+                    if x < y:
+                        yield next(xi)
+                    elif x == y:
+                        yield next(xi)
+                        next(yi)
+                    else:
+                        yield next(yi)
                 else:
-                    yield next(yi)
+                    yield from xi
+                    break
             else:
-                yield from xi
+                yield from yi
                 break
-        else:
-            yield from yi
-            break
 
-def longest_subseq(xs, ys):
-    lx = len(xs)
-    ly = len(ys)
+    @staticmethod
+    def unique_diff(zs, z):
+        d1,x1 = z
+        if d1 > 0:
+            return zs + [z]
+        lz = len(zs)
+        for i in range(lz-1,-1,-1):
+            d2,x2 = zs[i]
+            if d2 <= 0:
+                return zs[:i+1] + [z] + zs[i+1:]
+        return [z] + zs
 
-    memo = [[None for _ in range(ly+1)] for _ in range(lx+1)]
-    for i in range(lx+1):
-        memo[i][0] = [[]]
-    for j in range(ly+1):
-        memo[0][j] = [[]]
+    def compute(self):
+        lx = len(self.xs)
+        ly = len(self.ys)
 
-    for i,x in enumerate(xs):
-        for j,y in enumerate(ys):
-            if x == y:
-                memo[i+1][j+1] = [zs+[x] for zs in memo[i][j]]
-            else:
-                zss = list(mergeSet(memo[i+1][j], memo[i][j+1]))
-                lz = max(map(len, zss))
-                memo[i+1][j+1] = [zs for zs in zss if len(zs) == lz]
+        memo = [[None for _ in range(ly+1)] for _ in range(lx+1)]
+        for i in range(lx+1):
+            memo[i][0] = [[]]
+        for j in range(ly+1):
+            memo[0][j] = [[]]
 
-    return memo[lx][ly]
+        for i,x in enumerate(self.xs):
+            for j,y in enumerate(self.ys):
+                if x == y:
+                    memo[i+1][j+1] = [zs+[x] for zs in memo[i][j]]
+                else:
+                    zss = list(LCS.mergeSet(memo[i+1][j], memo[i][j+1]))
+                    lz = max(map(len, zss))
+                    memo[i+1][j+1] = [zs for zs in zss if len(zs) == lz]
+
+        self.memo = memo
+
+    def compute2(self):
+        # Hirschberg-esque algorithm
+        # uses O(min(m,n)) space rather than O(mn)
+        xs = self.xs
+        ys = self.ys
+
+        lx = len(xs)
+        ly = len(ys)
+
+        if lx < ly:
+            lx, ly = ly, lx
+            xs, ys = ys, xs
+
+        row = lambda: [[[]]] + [None]*ly
+        memo = [[[[]] for _ in range(ly+1)], row()]
+
+        for i,x in enumerate(xs):
+            if i > 0:
+                memo = [memo[1], row()]
+
+            for j,y in enumerate(ys):
+                if x == y:
+                    memo[1][j+1] = [zs+[x] for zs in memo[0][j]]
+                else:
+                    zss = list(LCS.mergeSet(memo[1][j], memo[0][j+1]))
+                    lz = max(map(len, zss))
+                    memo[1][j+1] = [zs for zs in zss if len(zs) == lz]
+
+        self.memo = memo[1]
+
+    def compute3(self):
+        # Hirschberg-esque algorithm
+        # uses O(min(m,n)) space rather than O(mn)
+        # we also compute the diff simultaneously
+
+        xs = self.xs
+        ys = self.ys
+
+        lx = len(xs)
+        ly = len(ys)
+
+        if lx < ly:
+            lx, ly = ly, lx
+            xs, ys = ys, xs
+
+        ud = LCS.unique_diff
+        row = lambda i: [[(0,[(-1,x) for x in xs[:i]])]] + [None]*ly
+        memo = [[[(0,[(+1,y) for y in ys[:j]])] for j in range(ly+1)], row(1)]
+
+        for i,x in enumerate(xs):
+            if i > 0:
+                memo = [memo[1], row(i+1)]
+
+            for j,y in enumerate(ys):
+                if x == y:
+                    memo[1][j+1] = [(n+1,zs+[(0,x)]) for (n,zs) in memo[0][j]]
+                else:
+                    zs1 = [(n,ud(zs, (+1,y))) for (n,zs) in memo[1][j]]
+                    zs2 = [(n,ud(zs, (-1,x))) for (n,zs) in memo[0][j+1]]
+                    zss = list(LCS.mergeSet(zs1, zs2))
+                    lz = max(n for n,_ in zss)
+                    memo[1][j+1] = [(n,zs) for (n,zs) in zss if n == lz]
+
+        self.memo = memo[1]
+
+    def common(self):
+        seqs = [[z for (d,z) in zs if d == 0] for _,zs in self.memo[-1]]
+        return seqs
+        
+        # uniq = []
+        # for seq in seqs:
+        #     if seq not in uniq:
+        #         uniq.append(seq)
+        # return uniq
+
+    def common_str(self):
+        return [''.join(zs) for _,zs in self.common()]
+
+    def __len__(self):
+        return self.memo[-1][-1][0]
+
+    def diff(self):
+        return [zs for _,zs in self.memo[-1]]
+        raise NotImplementedError
+
 
 class py3_cmp:
     def __eq__(self, other):
@@ -378,7 +432,7 @@ def cmp(a, b):
 def cmp_float(a, b, tolerance, tol_abs=None):
     if tol_abs is True:
         tol_abs = tolerance
-    
+
     try:
         if abs(a - b) / a > tolerance:
             return cmp(a,b)
